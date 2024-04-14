1c359aa7c6e4fa362300c0dc3ac4be4ebfd995ce
Zachary Ringwood <zringwood@gmail.com>
2024-04-14 11:46:43 -0400

    Basic Layout done

--- src/components/PromptDisplay.js
+++ src/components/PromptDisplay.js
@@ -3,7 +3,7 @@ import { useState } from "react"
 export default function PromptDisplay({text}){
     const [prompt, setPrompt] = useState(text)
     return (
-        <div style={{display:'flex',justifyContent:"center", alignItems:"center", border:"1px solid black", borderRadius:"4px", flex: '0 0 30%'}}>
+        <div style={{display:'flex',height:"30%",justifyContent:"center", alignItems:"center", border:"1px solid black", borderRadius:"4px", flex: '0 0 22%'}}>
             <span>
                 {prompt}        
             </span>
