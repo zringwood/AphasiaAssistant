6993f2b6122321533966bafa63e913305df076a8
Zachary Ringwood <zringwood@gmail.com>
2024-04-14 12:08:58 -0400

    Warning was bothering me

--- src/components/PromptDisplay.js
+++ src/components/PromptDisplay.js
@@ -1,7 +1,8 @@
 import { useState } from "react"
 
 export default function PromptDisplay({text}){
-    const [prompt, setPrompt] = useState(text)
+    
+    const [prompt] = useState(text)
     return (
         <div style={{display:'flex',height:"30%",justifyContent:"center", alignItems:"center", border:"1px solid black", borderRadius:"4px", flex: '0 0 22%'}}>
             <span>
