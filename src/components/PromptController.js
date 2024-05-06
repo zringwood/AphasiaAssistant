27c425b12601b460951a431f20876a12fcf82d02
Zachary Ringwood <zringwood@gmail.com>
2024-05-06 12:50:18 -0400

    Set up promptcontroller state

--- src/components/PromptController.js
+++ src/components/PromptController.js
@@ -1,21 +1,16 @@
 import PromptDisplay from './PromptDisplay';
 
+import { useState } from 'react';
+export default function PromptController() {
+    const [promptValues, setPromptValues] = useState([0,1,2,3,4,5,6,7,8,9,10,11])
 
-export default function PromptController({children}){
     return (
-        <div style={{display:"flex", flexGrow:1,flexDirection:"row",flexWrap:"wrap", width:"100%", rowGap:"8px", justifyContent:"space-evenly"}}>
-             <PromptDisplay text="Hello World"/>
-      <PromptDisplay text="Hello"/>
-      <PromptDisplay text="World"/>
-      <PromptDisplay text="Hello World"/>
-      <PromptDisplay text="Hello"/>
-      <PromptDisplay text="World"/>
-      <PromptDisplay text="Hello World"/>
-      <PromptDisplay text="Hello"/>
-      <PromptDisplay text="World"/>
-      <PromptDisplay text="Hello World"/>
-      <PromptDisplay text="Hello"/>
-      <PromptDisplay text="World"/>
+        <div style={{ display: "flex", flexGrow: 1, flexDirection: "row", flexWrap: "wrap", width: "100%", rowGap: "8px", justifyContent: "space-evenly" }}>
+            {
+                promptValues.forEach(e => {
+                    <PromptDisplay text="Hello World" />
+                })
+            }
         </div>
     )
 }
